012cf68519bc8ea27aafb7e09217abe79703c567
Dinesh V <dinesh.viswanath@nttdata.com>
2017-11-13 01:19:55 +0000

    [UPDATE] Changes in the Order creation JS function

--- public/javascripts/modules/confirmPayment.js
+++ public/javascripts/modules/confirmPayment.js
@@ -3,14 +3,17 @@ import { default as Web3 } from 'web3';
 import { default as contract } from 'truffle-contract';
 
 import metaCoinArtifacts from '../../../build/contracts/MetaCoin';
+import transactionArtifacts from '../../../build/contracts/Transaction';
 
 var accounts;
 var account;
 var MetaCoin = TruffleContract(metaCoinArtifacts);
+var DroneDrops = TruffleContract(transactionArtifacts);
 
 function init() {
 
 	MetaCoin.setProvider(web3.currentProvider);
+	DroneDrops.setProvider(web3.currentProvider);
 
 	web3.eth.getAccounts(function(err, accs) {
 		if (err != null) {
@@ -44,40 +47,32 @@ function placeOrder() {
 	console.log('Initiate Order');
 }
 
-function confirmPayment() {
-	// Initiate placeOrder here.
+function confirmPayment(){
 	init();
-	console.log('Confirm Payment Clicked');
+
+	console.log(`confirm Payment from account ${account}`);
+	DroneDrops.deployed().then(function(instance) {
+		var drone = instance.placeOrder(
+			"ls101ea", 
+			"ls1010lj",
+			123,
+			"0x14723a09acff6d2a60dcdf7aa4aff308fddc160c",
+			"0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db",
+			"0x583031d1113ad414f02576bd6afabfb302140225",
+			{ 
+				from: account,
+				value: 50000000000000000,
+				gas: 300000
+			}
+		);
+		return drone;
+	  }).then(function(value) {
+		console.log('Order created in the Blockchain!!!');
+		console.log(value.valueOf());
+	  }).catch(function(e) {
+		console.log(e);
+		console.log('Unable to create Order. Check above error.');
+	  });
 }
-
-
-/*
-async placeOrder() { 
-    try {
-      let pickupFrom = "Fenchurch Street, London";
-      let deliverTo = "Royal Exchange, London";
-      let droneId = "123";
-      let senderEth = web3.eth.accounts[1]; //0x7c5c40985b0f9e5fe69c02298a9482f429f7e725
-      let droneEth = web3.eth.accounts[2]; //0xbf5e980c1444f86e07311c330c806cd6516b20c4
-      let consumerEth = web3.eth.accounts[3]; //0x5e36edb1ba3d293932fc264c6d6afd389f024f39
-      web3.eth.defaultAccount =  web3.eth.coinbase;
-
-      const response = await fetch('../build/contracts/Transaction.json');
-      const Transaction_JSON = await response.json();
-      const Transaction = await TruffleContract(Transaction_JSON);
-      Transaction.setProvider(new Web3.providers.HttpProvider("http://localhost:8545"));
-
-      let meta = await Transaction.deployed();
-      let transactionStatus = await meta.placeOrder(pickupFrom, deliverTo, droneId, consumerEth, senderEth, droneEth,  {from: senderEth});
-      console.log('transactionStatus - ', transactionStatus);
-      // this.$.successToast.text = 'Transaction Complete';
-      // this.$.successToast.open();
-
-    } catch (error) {
-      // this.$.errorToast.text = `Error: ${error.message}`;
-      // this.$.errorToast.open();
-      console.log(`Error: ${error.message}`);
-    }
-} */
 
 export default confirmPayment;
