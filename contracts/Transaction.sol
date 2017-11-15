717a32c0af8113770deea786db70f1dcc2c78aa3
jerome <jeromes22@gmail.com>
2017-11-15 23:04:30 +0000

    order struct changes and additional validation

--- contracts/Transaction.sol
+++ contracts/Transaction.sol
@@ -1,24 +1,23 @@
 pragma solidity ^0.4.0;
 import "./OwnerAction.sol";
 //import "github-ethereum";
+//import "github.com/Arachnid/solidity-stringutils/strings.sol";
 
 contract Transaction is OwnerAction {
-    
+    //using strings for *;
     uint256 salt = 1;
-    mapping(uint256 => uint256) droneOrderMapping;
-
+    mapping(string => uint256) droneOrderMapping;
     struct Order {
         uint256 orderId;
         string pickupFrom; // Pickup Location of Drone. CSV of address,lat,lng
         string deliverTo; // Delivery Location CSV of address,lat,lng
         uint256 transactionPrice;
-        uint256  droneId;
+        string  droneId;
         address consumerEthAccount;
         address senderEthAccount;
         address droneEthAccount;
         Status status;
     }
-    
     Order[]  orderDetails;
     
     enum Status { Open, Closed }
@@ -27,15 +26,15 @@ contract Transaction is OwnerAction {
         owner = msg.sender;
     }
     
-   event OrderValidated(uint256 droneId, address consumerEth);
-    function validateOpenOrderStatus(uint256 droneId, address consumerEth) public payable  returns (uint orderId) {
+   event OrderValidated(string droneId, address consumerEth);
+    function validateOpenOrderStatus(string droneId, address consumerEth) public payable  returns (uint orderId) {
         var mappedOrderId = droneOrderMapping[droneId];
-      require(mappedOrderId >= 0);
-      Order currentOrder = orderDetails[mappedOrderId - salt];
+        require(mappedOrderId >= 0);
+        Order currentOrder = orderDetails[mappedOrderId - salt];
         require(currentOrder.orderId >= 0);
-        if(uint(currentOrder.status) == uint(Status.Open) && currentOrder.droneId == droneId) {
-               OrderValidated(droneId, consumerEth);
-                return currentOrder.orderId;
+        if(uint(currentOrder.status) == uint(Status.Open) && sha3(currentOrder.droneId) == sha3(droneId)) {
+            OrderValidated(droneId, consumerEth);
+            return currentOrder.orderId;
         }
         // TODO update mapping here?
         return 0;
@@ -43,7 +42,7 @@ contract Transaction is OwnerAction {
     
      // confirm-payment initiates this. 
     event OrderCreated(uint256 orderId, address consumerEthAccount);
-    function placeOrder(string pickupFrom, string deliverTo, uint256 droneId, address consumerEth, address senderEth, address droneEth) public payable returns (uint256 orderId) {
+    function placeOrder(string pickupFrom, string deliverTo, string droneId, address consumerEth, address senderEth, address droneEth) public payable returns (uint256 orderId) {
         if (msg.value <= 0) {
             throw;
         }
@@ -62,17 +61,15 @@ contract Transaction is OwnerAction {
         //owner.transfer(msg.value);
     }
     
-      
-    
-    event OrderStatusUpdated(uint256 orderId, address consumerEth, uint256 droneId, string nodeLocation);
-    function updateOrderStatus(uint256 orderId, uint256 droneId, address consumerEth, string nodeLocation) public {
+    event OrderStatusUpdated(uint256 orderId, address consumerEth, string droneId, string nodeLocation);
+    function updateOrderStatus(uint256 orderId, string droneId, address consumerEth, string nodeLocation) public {
         // TODO update mapping here?
         Order currentOrder = orderDetails[orderId-salt];
         require(Status.Open == currentOrder.status);
-        require(droneId == currentOrder.droneId);
+        sha3(droneId) == sha3(currentOrder.droneId);
         //require(nodeLocation == currentOrder.deliverTo);
         currentOrder.status = Status.Closed;
-        
+        //finaliseStake(currentOrder);
         address droneEthAccount = currentOrder.droneEthAccount;
         droneEthAccount.transfer(currentOrder.transactionPrice);
         OrderStatusUpdated(orderId, consumerEth, droneId, nodeLocation);
@@ -82,7 +79,6 @@ contract Transaction is OwnerAction {
         address droneEthAccount = currentOrder.droneEthAccount;
         droneEthAccount.transfer(currentOrder.transactionPrice);
     }*/
-    
     
     event OrderReverted(uint256 orderId, address consumerEth);
     function revertPlacedOrder(uint256 orderId, address consumerEth) public {
@@ -101,7 +97,5 @@ contract Transaction is OwnerAction {
     function destroy() public ownerAction {
         selfdestruct(owner);
     }
-    
 
-  
 }
