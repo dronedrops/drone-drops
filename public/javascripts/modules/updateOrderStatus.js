89a32d0d548521e2e52ffde65e24aebe5b37aeaa
jerome <jeromes22@gmail.com>
2017-11-15 18:06:33 +0000

    updated contract as per the latest requirement.

---
+++ public/javascripts/modules/updateOrderStatus.js
@@ -0,0 +1,41 @@
+import { $ } from './bling';
+import { default as Web3 } from 'web3';
+import { default as contract } from 'truffle-contract';
+
+import transactionArtifacts from '../../../build/contracts/Transaction';
+
+var accounts;
+var account;
+var order;
+var DroneDrops = TruffleContract(transactionArtifacts);
+
+function init() {
+    DroneDrops.setProvider(web3.currentProvider);
+    web3.eth.getAccounts(function(err, accs) {
+        if (err != null) {
+            alert('There was an error fetching your accounts.');
+            return;
+        }
+        if (accs.length == 0) {
+            alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+            return;
+        }
+        accounts = accs;
+        account = accounts[0];
+        console.log(account);
+    });
+}
+
+function updateOrderStatus() {
+    init();
+    console.log(`Updating order details ${account}`);
+    DroneDrops.deployed().then(function(instance) {
+        var result = instance.validateOpenOrder(1, 123, accounts[3]);
+        return result;
+    }).then(function(value) {
+        console.log('Order validated from the Blockchain!!!');
+        console.log(value);
+    }).catch(function(e) {
+        console.log('Unable to validate created Order', e);
+    });
+}
