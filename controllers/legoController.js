04efe382a202ae8a390901d2406e665fc5e9e4ca
jerome <jeromes22@gmail.com>
2017-11-20 13:17:18 +0100

    fixed pi npm case sensitive issue.

--- controllers/legoController.js
+++ controllers/legoController.js
@@ -1,4 +1,4 @@
-var Wedo2 = require('WeDo2');
+var Wedo2 = require('wedo2');
 var wedo2 = new Wedo2('LPF2');
 var identifyDrone = require('./identifyDrone');
 var flyMambo = require('./flyMambo');
@@ -10,82 +10,82 @@ var deliverToPostCode = 'LS101EA';
 var API_BASE_URL = 'http://localhost:7777/api';
 
 wedo2.on('connected', function(uuid) {
-	console.log('I found a WeDo with uuid: ' + uuid);
-	// Place getters and setters in here, to make sure that they are called,
-	// when the object is connected
-	wedo2.getDeviceName(function(name, uuid) {
-		console.log('the device name is ' + name + ' @ ' + uuid);
-	}, uuid);
+    console.log('I found a WeDo with uuid: ' + uuid);
+    // Place getters and setters in here, to make sure that they are called,
+    // when the object is connected
+    wedo2.getDeviceName(function(name, uuid) {
+        console.log('the device name is ' + name + ' @ ' + uuid);
+    }, uuid);
 });
 
 wedo2.on('battery', function(status, uuid) {
-	console.log('Battery: ' + status + '% @ ' + uuid);
+    console.log('Battery: ' + status + '% @ ' + uuid);
 });
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 async function openDoor(uuid) {
-	console.log('this might take some time....');
-	wedo2.setMotor(100, 1, uuid);
-	await delay(4000);
-	console.log('done!');
-	wedo2.setMotor(0, 1, uuid);
+    console.log('this might take some time....');
+    wedo2.setMotor(100, 1, uuid);
+    await delay(4000);
+    console.log('done!');
+    wedo2.setMotor(0, 1, uuid);
 }
 
 async function getOpenOrderId(droneUUID, consumerEth) {
-	try {
-		let orderStatus = await axios(`${API_BASE_URL}/validateOrder?droneId=${droneUUID}&consumerEth=${consumerEth}`);
-		let orderId = orderStatus.data.orderId;
-		console.log('getOpenOrderId', orderId);
-		return orderId;
-	} catch (error) {
-		console.error(error);
-	}
+    try {
+        let orderStatus = await axios(`${API_BASE_URL}/validateOrder?droneId=${droneUUID}&consumerEth=${consumerEth}`);
+        let orderId = orderStatus.data.orderId;
+        console.log('getOpenOrderId', orderId);
+        return orderId;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 async function updateOrderStatus(orderId, droneUUID) {
-	try {
-		let updateOrderStatus = await axios(
-			`${API_BASE_URL}/updateOrderStatus?orderId=${orderId}&droneId=${droneUUID}&consumerEth=${consumerEth}&deliverToPostCode=${deliverToPostCode}`
-		);
-		console.log('orderStatus', updateOrderStatus.data.orderStatus);
-	} catch (error) {
-		console.error(error);
-	}
+    try {
+        let updateOrderStatus = await axios(
+            `${API_BASE_URL}/updateOrderStatus?orderId=${orderId}&droneId=${droneUUID}&consumerEth=${consumerEth}&deliverToPostCode=${deliverToPostCode}`
+        );
+        console.log('orderStatus', updateOrderStatus.data.orderStatus);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 function closeDoor() {
-	//TODO: console.log('Close the door');
+    //TODO: console.log('Close the door');
 }
 
 wedo2.on('distanceSensor', function(distance, port, uuid) {
-	console.log('distanceSensor: ' + distance + ' at port ' + port + ' @ ' + uuid);
-	if (distance < 10) {
-		let droneUuid = identifyDrone.scanBleDevices();
-		let orderId = getOpenOrderId(droneUuid, consumerEth);
-		if (ordeId > 0) {
-			openDoor(uuid);
-			updateOrderStatus(ordeId, droneUuid);
-			flyMambo.dropPackage();
-			flyMambo.flyBack();
-			closeDoor();
-		} else {
-			// TODO: Add Danger Sound or RED LED
-			// TODO: Escrow Managment - Release the money.
-			flyMambo.flyBack();
-		}
-	}
+    console.log('distanceSensor: ' + distance + ' at port ' + port + ' @ ' + uuid);
+    if (distance < 10) {
+        let droneUuid = identifyDrone.scanBleDevices();
+        let orderId = getOpenOrderId(droneUuid, consumerEth);
+        if (ordeId > 0) {
+            openDoor(uuid);
+            updateOrderStatus(ordeId, droneUuid);
+            flyMambo.dropPackage();
+            flyMambo.flyBack();
+            closeDoor();
+        } else {
+            // TODO: Add Danger Sound or RED LED
+            // TODO: Escrow Managment - Release the money.
+            flyMambo.flyBack();
+        }
+    }
 });
 
 wedo2.on('port', function(port, connected, type, uuid) {
-	if (connected && getOpenOrderId() > 0) {
-		console.log('Found ' + type + ' on port ' + port + ' @ ' + uuid);
-		openDoor(uuid);
-	} else {
-		console.log('Disconnected ' + type + ' on port ' + port + ' @ ' + uuid);
-	}
+    if (connected && getOpenOrderId() > 0) {
+        console.log('Found ' + type + ' on port ' + port + ' @ ' + uuid);
+        openDoor(uuid);
+    } else {
+        console.log('Disconnected ' + type + ' on port ' + port + ' @ ' + uuid);
+    }
 });
 
 wedo2.on('disconnected', function(uuid) {
-	console.log('I removed a WeDo with uuid: ' + uuid);
+    console.log('I removed a WeDo with uuid: ' + uuid);
 });
